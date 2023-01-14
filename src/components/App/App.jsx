6e9a5350b9800b178a4fe0bd4cabf48c1d33de59
Adam Bergstrom <abx1138@gmail.com>
2023-01-14 16:19:31 -0600

    added GalleryItem, GalleryList, updated app.jsx GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [trolls, setTrolls] = useState([]);
+
+  // Run some code ONE TIME when this component
+  // "mounts" on the DOM:
+  useEffect(() => {
+    fetchTrolls();
+  }, []); // Empty Array here is the stop sign 🛑
+
+  const fetchTrolls = () => {
+    // Ask our server for the creatures data:
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log('in GET /gallery', response)
+      // Set our creatures React state variable to
+      // have the value of our array of creature objects:
+      const trolls = response.data;
+      setTrolls(trolls);
+    }).catch((error) => {
+      console.log('fetchTrolls error:', error);
+    })
+  }
+
+
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          {/* <h1 className="App-title">Gallery of My Life</h1> */}
+          <h1 className="App-title">Gallery of Trolls</h1>
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
